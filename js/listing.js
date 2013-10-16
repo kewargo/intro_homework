ec65ecc0fd01a36449bef1c42dc4ec69db90fdaf
Kerry Wargo <kewargo@verizon.net>
2013-10-15 20:11:51 -0400

    Completed Homework 1

---
+++ js/listing.js
@@ -0,0 +1,14 @@
+var tasks = ['Change oil in the Ram 2500', 'Change Oil in Subaru Forrester', 
+'Change Oil in Mini Cooper', 'Change air filter in HVAC Air Handler', 
+'Change batteries in Carbon Dioxide Detectors', 
+'Bleed air out of hot water radiators', 
+'Change water filter in refrigerator'];
+
+document.write('<ul class="list-group">');
+
+for(var i=0; i < tasks.length; i++) {
+
+    document.write('<li class="list-group-item"><span class="glyphicon glyphicon-637-expand.png"></span>   ' + to_dos[i] + '</li>'); 
+}    
+
+document.write('</ul>');
