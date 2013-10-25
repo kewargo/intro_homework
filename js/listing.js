ad7b409d02c9a784076da8aca982c0d5a42f8718
Kerry Wargo <kewargo@verizon.net>
2013-10-24 21:49:52 -0400

    Homework #2 with new glyphs & error handling

--- js/listing.js
+++ js/listing.js
@@ -1,29 +1,45 @@
 
-var tasks = [{taskDesc: 'Change oil in the Ram 2500', completed: false}, 
-{taskDesc: 'Change Oil in Subaru Forrester', completed: false}, 
-{taskDesc: 'Change Oil in Mini Cooper', completed: true}, {taskDesc: 'Change air filter in HVAC Air Handler', completed: true}, 
-{taskDesc: 'Change batteries in Carbon Dioxide Detectors', completed: false}, 
-{taskDesc: 'Bleed air out of hot water radiators', completed: true}, 
-{taskDesc: 'Change water filter in refrigerator', completed: false}];
+var tasks = [{'taskDesc':'Change oil in the Ram 2500','completed':false}, 
+{'taskDesc':'Change Oil in Subaru Forrester','completed':false},
+{'taskDesc':'Change Oil in Mini Cooper','completed':true},
+{'taskDesc':'Change air filter in HVAC Air Handler','completed':true},
+{'taskDesc':'Change batteries in Carbon Dioxide Detectors','completed':false}, 
+{'taskDesc':'Bleed air out of hot water radiators','completed':true}, 
+{'taskDesc':'Change water filter in refrigerator','completed':false}];
 
-    for(var i = 0; i < tasks.length; i++)  
+document.write('<ul class="list-group">');
 
-        var name;
-        for(name in tasks) {
-            
-            if(typeof tasks[name] == 'object') {
-            
-                var subName;
-                   for(subName in tasks[name]) {
-                        document.write('<li class="list-group-item"><span class="glyphicon glyphicon-expand"></span> ' + tasks[name][subName] + '</li>');
-                        
-                    } // end for(subName in tasks)
-                
-            } else {
-                document.write(name + ": " + tasks[name]); 
-            
-        } // end for(name in tasks)
-      
-    } // end for(var i)
+ for(var i=0; i < tasks.length; i++) {
 
+/* try-catch logic for error handling, otherwise print tasks */
+
+    try {
+        printTask(tasks[i]);
+        } catch(error) {
+            console.log("There was an error: " + error);
+        }
+    }    
+
+ 
 document.write('</ul>');
+
+
+/* function to print tasks */
+
+function printTask(task) {
+    
+if(typeof task.taskDesc == 'undefined') {
+        throw new Error('This task is missing a description!');
+    }
+    
+    if( typeof task.completed == 'undefined' ) {
+        throw 'This task is missing a completion status!';
+        
+    }
+    
+        if(task.completed) {
+            document.write('<li class="list-group-item success">' + '<span class="glyphicon glyphicon-ok"></span>   ' + task.taskDesc + '</li>'); 
+          } else {
+            document.write('<li class="list-group-item">' + '<span class="glyphicon glyphicon-pushpin"></span>   ' + task.taskDesc + '</li>'); 
+        }
+    }
